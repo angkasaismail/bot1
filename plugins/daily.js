872f98bda0b46d8a2ad0a9b28737a646a77c4f9c
IG@Ismailreal.id <77217686+ismailofficial@users.noreply.github.com>
2021-11-24 18:16:26 +0700

    Update daily.js

--- plugins/daily.js
+++ plugins/daily.js
@@ -1,5 +1,5 @@
 const free = 500
-const prem = 5000
+const prem = 999999999
 let handler = async (m, { conn, usedPrefix, isPrems }) => {
   if (db.data.users[m.sender].level < 1) return await conn.sendButton(m.chat, 'raise your level', watermark, 'Level Up', `${usedPrefix}levelup`, m)
   let time = db.data.users[m.sender].lastclaim + 86400000
